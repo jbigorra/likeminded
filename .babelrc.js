76149b55120914f3bb007230ff7cd3c48292ad48
Juan Bigorra <juanchoibf@gmail.com>
2020-09-27 17:02:54 +0200

    Add misssing bable plugins and adjust config and base setup

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,9 @@
 const isTestEnv = process.env.NODE_ENV === 'test';
 
 module.exports = {
+  "plugins": ["@babel/plugin-transform-runtime"],
   "presets": [
     ["@babel/preset-env", { modules: isTestEnv ? 'commonjs' : false }],
-    "@babel/preset-react"
+    "@babel/preset-react",
   ]
 }
