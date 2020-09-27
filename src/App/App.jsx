6bf72814c24b56304571a93335aa47376c4e93d5
Juan Bigorra <juanchoibf@gmail.com>
2020-09-27 17:03:14 +0200

    Render registration form in App component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,10 @@
+import React from 'react';
+import { RegistrationForm } from './RegistrationForm/RegistrationForm';
 
 function App () {
-  return null;
+  return <>
+    <RegistrationForm/>
+  </>;
 }
 
 export default App;
